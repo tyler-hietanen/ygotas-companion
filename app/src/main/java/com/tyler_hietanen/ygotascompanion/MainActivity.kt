bd63e9e8e9607e9aa5509d30d84f0e7617727046
Tyler Hietanen <thietanen21@live.com>
2025-06-22 16:19:01 -0500

    Added basic compose ui.

---
+++ app/src/main/java/com/tyler_hietanen/ygotascompanion/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.tyler_hietanen.ygotascompanion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tyler_hietanen.composeexample.ui.theme.ComposeExampleTheme
+
+class MainActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreetingPreview()
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier)
+{
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview()
+{
+    ComposeExampleTheme {
+        Greeting("Android")
+    }
+}
